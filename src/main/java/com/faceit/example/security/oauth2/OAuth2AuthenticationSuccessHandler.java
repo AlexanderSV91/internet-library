f48e88ca3fa62f21abdd6b75e5cb0c6e5996cb1d
AlexanderSV91 <alex.shalamov4@gmail.com>
2021-03-12 14:32:08 +0200

    default redirect uri

--- src/main/java/com/faceit/example/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/faceit/example/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -53,7 +53,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                     "Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
         }
 
-        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl() + "user");
         String token = tokenProvider.createToken(authentication);
         return UriComponentsBuilder
                 .fromUriString(targetUrl)
